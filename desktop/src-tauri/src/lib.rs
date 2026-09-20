31adaf541c71b4f02ee2b08088508efc8f6f6cfe
jalejandrov93 <jhonvasquez93@utp.edu.co>
2026-09-20 16:31:40 -0500

    feat(desktop): add native tray menu and window hide-on-close

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,3 +1,9 @@
+use tauri::{
+    menu::MenuBuilder,
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    Emitter, Manager, WindowEvent,
+};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -6,7 +12,88 @@ pub fn run() {
             tauri_plugin_autostart::MacosLauncher::LaunchAgent,
             Some(vec![]),
         ))
-        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app
+                .get_webview_window("main")
+                .or_else(|| app.webview_windows().into_values().next())
+            {
+                let _ = window.show();
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
+        .setup(|app| {
+            let tray_menu = MenuBuilder::new(app)
+                .text("open", "Abrir Mascota")
+                .separator()
+                .text("pause", "Pausar monitoreo")
+                .text("restart", "Reiniciar servicios")
+                .separator()
+                .text("quit", "Salir")
+                .build()?;
+
+            TrayIconBuilder::new()
+                .icon(tauri::include_image!("icons/32x32.png"))
+                .menu(&tray_menu)
+                .show_menu_on_left_click(false)
+                .on_menu_event(|app, event| {
+                    match event.id.as_ref() {
+                        "open" => {
+                            if let Some(window) = app
+                                .get_webview_window("main")
+                                .or_else(|| app.webview_windows().into_values().next())
+                            {
+                                let _ = window.show();
+                                let _ = window.unminimize();
+                                let _ = window.set_focus();
+                            }
+                        }
+                        "pause" => {
+                            let _ = app.emit("moka-pause", ());
+                        }
+                        "restart" => {
+                            let _ = app.emit("moka-restart-services", ());
+                        }
+                        "quit" => {
+                            app.exit(0);
+                        }
+                        _ => {}
+                    }
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        let app = tray.app_handle();
+                        if let Some(window) = app
+                            .get_webview_window("main")
+                            .or_else(|| app.webview_windows().into_values().next())
+                        {
+                            let is_visible = window.is_visible().unwrap_or(false);
+                            if is_visible {
+                                let _ = window.hide();
+                            } else {
+                                let _ = window.show();
+                                let _ = window.unminimize();
+                                let _ = window.set_focus();
+                            }
+                        }
+                    }
+                })
+                .build(app)?;
+
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                api.prevent_close();
+                let _ = window.hide();
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
